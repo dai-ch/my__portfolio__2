444378057869b0b025086e6bc20331721fe570e3
Miu <miu.taji@gmail.com>
2020-07-26 22:33:50 +0900

    REVIEW: ProfileセクションのCSS（SP）

--- js/main.js
+++ js/main.js
@@ -2,17 +2,18 @@
 
 ** スマホメニュー
 ==================================================*/
-(function() {
+(function () {
   const body = document.querySelector('body');
   const menuBtn = document.querySelector('#menu-btn');
   const gnav = document.querySelector('#gnav');
+  const items = document.querySelectorAll('#gnav a');
   const classToAssign = 'is-open-gnav';
 
   function manipulateGnav(e) {
     e.stopPropagation();
     gnav.style.transition = '';
 
-    if(body.classList.contains(classToAssign)) {
+    if (body.classList.contains(classToAssign)) {
       body.classList.remove(classToAssign);
     } else {
       body.classList.add(classToAssign);
@@ -20,12 +21,17 @@
   }
 
   function closeGnav() {
+    gnav.style.transition = 'none';
+    body.classList.remove(classToAssign);
+  }
+
+  function fetchWindowWidth() {
     if(window.innerWidth > 1024) {
-      gnav.style.transition = 'none';
-      body.classList.remove(classToAssign);
+      closeGnav();
     }
   }
 
   menuBtn.addEventListener('click', manipulateGnav);
-  window.addEventListener('resize', closeGnav);
+  window.addEventListener('resize', fetchWindowWidth);
+  items.forEach(item => item.addEventListener('click', closeGnav));
 })();
