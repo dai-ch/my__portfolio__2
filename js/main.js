1f647a14d2ee4687ad7a348bccdcbdaa68878cc7
daisuke-c <09p061@gmail.com>
2020-08-08 22:59:50 +0900

    worksセクションにカラーボックス実装（失敗）

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@
 ** スマホメニュー
 ==================================================*/
 (function () {
-  const body = document.querySelector('body');
-  const menuBtn = document.querySelector('#menu-btn');
-  const gnav = document.querySelector('#gnav');
-  const items = document.querySelectorAll('#gnav a');
-  const classToAssign = 'is-open-gnav';
+  const body = document.querySelector("body");
+  const menuBtn = document.querySelector("#menu-btn");
+  const gnav = document.querySelector("#gnav");
+  const items = document.querySelectorAll("#gnav a");
+  const classToAssign = "is-open-gnav";
 
   function manipulateGnav(e) {
     e.stopPropagation();
-    gnav.style.transition = '';
+    gnav.style.transition = "";
 
     if (body.classList.contains(classToAssign)) {
       body.classList.remove(classToAssign);
@@ -21,19 +21,19 @@
   }
 
   function closeGnav() {
-    gnav.style.transition = 'none';
+    gnav.style.transition = "none";
     body.classList.remove(classToAssign);
   }
 
   function fetchWindowWidth() {
-    if(window.innerWidth > 1024) {
+    if (window.innerWidth > 1024) {
       closeGnav();
     }
   }
 
-  menuBtn.addEventListener('click', manipulateGnav);
-  window.addEventListener('resize', fetchWindowWidth);
-  items.forEach(item => item.addEventListener('click', closeGnav));
+  menuBtn.addEventListener("click", manipulateGnav);
+  window.addEventListener("resize", fetchWindowWidth);
+  items.forEach((item) => item.addEventListener("click", closeGnav));
 })();
 
 /*==================================================
@@ -42,12 +42,13 @@
 ==================================================*/
 (function () {
   const aHashes = document.querySelectorAll('a[href^="#"]');
-  const header = document.querySelector('#header');
-	const duration = 300;
-	let headerHeight;
+  const header = document.querySelector("#header");
+  const duration = 300;
+  let headerHeight;
 
-	let Ease = {
-		easeInOut: t => t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
+  let Ease = {
+    easeInOut: (t) =>
+      t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
   };
 
   function fetchHeaderHeight() {
@@ -55,35 +56,42 @@
     return headerHeight;
   }
 
-	function move(event) {
-		const currentY = document.documentElement.scrollTop || document.body.scrollTop;
-		const targetID = event.target.hash.replace(/#/, '');
-		const target = document.getElementById(targetID);
+  function move(event) {
+    const currentY =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    const targetID = event.target.hash.replace(/#/, "");
+    const target = document.getElementById(targetID);
 
-		if (target) {
-			event.preventDefault();
-			event.stopPropagation();
+    if (target) {
+      event.preventDefault();
+      event.stopPropagation();
 
-			const targetY = window.pageYOffset + target.getBoundingClientRect().top - headerHeight;
-			const startTime = performance.now();
+      const targetY =
+        window.pageYOffset + target.getBoundingClientRect().top - headerHeight;
+      const startTime = performance.now();
 
-			const loop = (nowTime) => {
-				const time = nowTime - startTime;
-				const normalizedTime = time / duration;
+      const loop = (nowTime) => {
+        const time = nowTime - startTime;
+        const normalizedTime = time / duration;
 
-				if (normalizedTime < 1) {
-					window.scrollTo(0, currentY + ((targetY - currentY) * Ease.easeInOut(normalizedTime)));
-					requestAnimationFrame(loop);
-				} else {
-					window.scrollTo(0, targetY);
-				}
-			};
+        if (normalizedTime < 1) {
+          window.scrollTo(
+            0,
+            currentY + (targetY - currentY) * Ease.easeInOut(normalizedTime)
+          );
+          requestAnimationFrame(loop);
+        } else {
+          window.scrollTo(0, targetY);
+        }
+      };
 
-			requestAnimationFrame(loop);
-		}
-	}
+      requestAnimationFrame(loop);
+    }
+  }
 
-  window.addEventListener('load', fetchHeaderHeight);
-  window.addEventListener('resize', fetchHeaderHeight);
-	aHashes.forEach(a => a.addEventListener('click', { event: a.target, handleEvent: move }, false));
+  window.addEventListener("load", fetchHeaderHeight);
+  window.addEventListener("resize", fetchHeaderHeight);
+  aHashes.forEach((a) =>
+    a.addEventListener("click", { event: a.target, handleEvent: move }, false)
+  );
 })();
